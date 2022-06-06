cf93b5f59f42accae10d15c84acfea34a9cd1b1b
Vinod <72577940+vinod1147@users.noreply.github.com>
2022-06-06 23:12:06 +0530

    Creating Backend

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ import { useRef } from 'react';
 
 import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from 'react-icons/ai';
 import { BsFillBagCheckFill } from 'react-icons/bs';
+import { MdAccountCircle } from 'react-icons/md';
 
 const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
 
@@ -40,8 +41,11 @@ const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
                     <Link href={'/stickers'}><a><li>Stickers</li></a></Link>
                 </ul >
             </div >
-            <div onClick={toogleCart} className="cursor-pointer cart absolute right-0 top-4 mx-5">
-                <AiOutlineShoppingCart className='text-xl md:text-2xl' />
+            <div className="cursor-pointer cart absolute right-0 top-4 mx-5 flex">
+                <Link href={'/login'}>
+                    <MdAccountCircle className='text-xl md:text-2xl mx-2' />
+                </Link>
+                <AiOutlineShoppingCart onClick={toogleCart} className='text-xl md:text-2xl' />
             </div>
             <div ref={ref} className={`w-72 h-[100vh] sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0' : 'translate-x-full'} `}>
                 <h2 className='font-bold text-xl text-center'>Shoping Cart</h2>
--- pages/forgot.js
+++ pages/forgot.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Signup = () => {
+const Forgot = () => {
     return (
         <div>
             <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-md w-full space-y-8">
                     <div>
                         <img className="mx-auto h-12 w-auto" src="/logo.png" alt="Workflow" />
-                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create New Account</h2>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">This is page for forgot password this is remain to design</h2>
                         <p className="mt-2 text-center text-sm text-gray-600">
                             Or
-                            <Link href={'/login'}>
-                                <a href="#" className="font-medium text-pink-600 hover:text-pink-500"> Sign In </a>
+                            <Link href={'/signup'}>
+                                <a href="#" className="font-medium text-pink-600 hover:text-pink-500"> Sign Up </a>
                             </Link>
                         </p>
                     </div>
@@ -20,20 +20,25 @@ const Signup = () => {
                         <input type="hidden" name="remember" value="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
-                                <label htmlFor="name" className="sr-only">Name</label>
-                                <input id="name" name="name" type="text" autoComplete="name" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm" placeholder="Name" />
-                            </div>
-                            <div>
                                 <label htmlFor="email-address" className="sr-only">Email address</label>
-                                <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm" placeholder="Email address" />
-                            </div>
-                            <div>
-                                <label htmlFor="phone" className="sr-only">Phone Number</label>
-                                <input id="phone" name="phone" type="phone" autoComplete="phone" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm" placeholder="Phone Number" />
+                                <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm" placeholder="Email address" />
                             </div>
                             <div>
                                 <label htmlFor="password" className="sr-only">Password</label>
                                 <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-pink-500 focus:border-pink-500 focus:z-10 sm:text-sm" placeholder="Password" />
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center">
+                                <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-pink-600 focus:ring-pink-500 border-gray-300 rounded" />
+                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900"> Remember me </label>
+                            </div>
+
+                            <div className="text-sm">
+                                <Link href={'/forgot'}>
+                                    <a href="#" className="font-medium text-pink-600 hover:text-pink-500"> Forgot your password? </a>
+                                </Link>
                             </div>
                         </div>
 
@@ -45,7 +50,7 @@ const Signup = () => {
                                         <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                                     </svg>
                                 </span>
-                                Sign Up
+                                Sign in
                             </button>
                         </div>
                     </form>
@@ -55,4 +60,4 @@ const Signup = () => {
     );
 }
 
-export default Signup;
+export default Forgot;
