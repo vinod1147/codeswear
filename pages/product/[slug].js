a2916ff93db9041e3adc819a936efa07116e9ae6
Vinod <72577940+vinod1147@users.noreply.github.com>
2022-06-11 01:03:19 +0530

    Customized the Hoodies Page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -99,9 +99,9 @@ const Post = ({ addToCart, product, variants, buyNow }) => {
                             </div>
                         </div >
                         <div className="flex">
-                            <span className="title-font font-medium text-2xl text-gray-900">₹499</span>
-                            <button onClick={() => { buyNow(slug, 1, 499, product.title, size, color) }} className="flex ml-8 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">Buy Now</button>
-                            <button onClick={() => { addToCart(slug, 1, 499, product.title, size, color) }} className="flex ml-4 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">Add to Cart</button>
+                            <span className="title-font font-medium text-2xl text-gray-900">₹{product.price}</span>
+                            <button onClick={() => { buyNow(slug, 1, product.price, product.title, size, color) }} className="flex ml-8 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">Buy Now</button>
+                            <button onClick={() => { addToCart(slug, 1, product.price, product.title, size, color) }} className="flex ml-4 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">Add to Cart</button>
                             <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
@@ -127,7 +127,7 @@ export async function getServerSideProps(context) {
     }
     await mongoose.connect(process.env.MONGO_URI)
     let product = await Product.findOne({ slug: context.query.slug })
-    let variants = await Product.find({ title: product.title })
+    let variants = await Product.find({ title: product.title, category: product.category })
     let colorSizeSlug = {}
     for (let item of variants) {
         if (Object.keys(colorSizeSlug).includes(item.color)) {
