4036ffb5c7c11f438f2bb16301ce33bbb08672b3
Vinod <72577940+vinod1147@users.noreply.github.com>
2022-06-13 01:18:15 +0530

    Added Encryption

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,15 +2,18 @@
 
 import User from "../../models/User"
 import connectDb from "../../middleware/mongoose"
+var CryptoJS = require("crypto-js");
+
 
 const handler = async (req, res) => {
     if (req.method == 'POST') {
-        console.log(req.body);
-
         let user = await User.findOne({ "email": req.body.email })
 
+        const bytes = CryptoJS.AES.decrypt(user.password, 'secret123');
+        let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
+
         if (user) {
-            if (req.body.email == user.email && req.body.password == user.password) {
+            if (req.body.email == user.email && req.body.password == decryptedPass) {
                 res.status(200).json({ success: true, email: user.email, name: user.name })
             } else {
                 res.status(200).json({ success: false, error: "Invalid Credentials" })
